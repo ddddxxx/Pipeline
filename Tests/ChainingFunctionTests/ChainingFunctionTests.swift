498d4b1a64183b8dc7b5b3c5f324f61252ff43da
Xander Deng <dengxiang2010@gmail.com>
2017-12-13 10:03:51 +0800

    test: optional argument chaining

--- Tests/ChainingFunctionTests/ChainingFunctionTests.swift
+++ Tests/ChainingFunctionTests/ChainingFunctionTests.swift
@@ -2,15 +2,20 @@ import XCTest
 @testable import ChainingFunction
 
 class ChainingFunctionTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ChainingFunction().text, "Hello, World!")
+
+    func testOptionalArgumentChaining() {
+        func f(_ data: Data?) -> Int? {
+            return (data, String.Encoding.utf8) ?> String.init(data:encoding:) ?> Double.init ?> Int.init(exactly:)
+        }
+        
+        XCTAssertNil(f(nil))
+        XCTAssertNil(f("a".data(using: .utf8)))
+        XCTAssertNil(f("0.1".data(using: .utf8)))
+        XCTAssertEqual(f("1".data(using: .utf8)), 1)
     }
 
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testOptionalArgumentChaining", testOptionalArgumentChaining),
     ]
 }
